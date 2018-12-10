725d765e7a32e1c2ae9ae55f80b56cfbe9b91134
lorethefixer <lore007@hotmail.it>
2018-12-10 09:19:32 +0100

    Uploaded Local Project

---
+++ Compito/Compito.cpp
@@ -0,0 +1,13 @@
+// Compito.cpp: Lorenzo Filippelli 10-12-18
+#include "stdafx.h"
+#include "iostream"
+using namespace std;
+
+int main()
+{
+	for (int i = 0; i < 10; i++) {
+		cout << "Contatore: " << i << endl;
+	}
+	system("PAUSE");
+}
+
