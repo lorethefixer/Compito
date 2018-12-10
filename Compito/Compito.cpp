b6b886302e76bd2d9cf97ba8764c149e20afce01
lorethefixer <lore007@hotmail.it>
2018-12-10 09:49:09 +0100

    fixed another comment

--- Compito/Compito.cpp
+++ Compito/Compito.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-	//for cycle that print the j variable on screen
+	//for cycle that print the j variable
 	for (int j = 0; j < 11; j++) {
 		//Print variable j
 		cout << "Counter: " << j << endl;
